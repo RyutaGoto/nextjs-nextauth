344c0643cc2459114c46d5f1ffb2915c9dc5376e
RyutaGoto <b1016230@fun.ac.jp>
2022-05-18 00:07:26 +0900

    formatted

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import '../styles/globals.css'
+import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
-import { SessionProvider } from 'next-auth/react';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-  return(
+  return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>

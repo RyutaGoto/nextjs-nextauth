0d4de41a86515a50132af0d1692f9f2b3667538c
RyutaGoto <b1016230@fun.ac.jp>
2022-05-18 23:02:57 +0900

    session log

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { useSession, signIn, signOut } from 'next-auth/react'
 
 const Home: NextPage = () => {
   const { data: session } = useSession()
+  console.log('session: ', session)
   if (session) {
     return (
       <>
